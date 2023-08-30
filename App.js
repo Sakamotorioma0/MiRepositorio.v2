2fd406312654da8a79c0653c63cba97df6911bbb
CarlosHG <38302476+Sakamotorioma0@users.noreply.github.com>
2023-08-30 17:09:30 -0600

    Add files via upload

---
+++ App.js
@@ -0,0 +1,16 @@
+// import es la parte principal devido que debe asegurarse de que react este disponible 
+import React from 'react';
+// importamos librerias de react
+// con react se puede utilizar la sintaxis de JSX (javascript XML)
+function App(){
+
+    // utilizamos return para dar salida al codigo html  en ingresar las etiquetas div contenedoras del texto h1
+    return (
+        <div>
+            <h1>primer texto ingresado</h1>
+        </div>
+    );
+}
+
+export default App;
+// se envia 
